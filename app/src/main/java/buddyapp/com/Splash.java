bddc831b082e34b47b0d161876be5df9a70cd457
Ajay <ajay.k@titechnologies.org>
2017-07-17 18:40:38 +0530

    added google lib

--- app/src/main/java/buddyapp/com/Splash.java
+++ app/src/main/java/buddyapp/com/Splash.java
@@ -1,7 +1,13 @@
 package buddyapp.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import buddyapp.com.activity.MainActivity;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 3000);
+
     }
 }
