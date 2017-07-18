5ac0f50fd03fc34e6ba985267f28c2296466f3a5
Aneesh P V <aneesh.pv@titechnologies.in>
2017-07-18 14:05:55 +0530

    layout changes

--- app/src/main/java/buddyapp/com/activity/RegisterScreen.java
+++ app/src/main/java/buddyapp/com/activity/RegisterScreen.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -16,6 +18,10 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import com.hbb20.CountryCodePicker;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,14 +30,20 @@ import buddyapp.com.R;
 import buddyapp.com.Settings.Constants;
 
 public class RegisterScreen extends AppCompatActivity {
-    TextView Google, facebook;
+    TextView Google, facebook, next;
+    EditText mobile;
     LoginButton facebook_loginbutton;
     CallbackManager callbackManager;
-    String email, fname, lname, gender;
+    String email, fname, lname, gender, countrycode, mobilenumber;
+    CountryCodePicker ccp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        next = (TextView) findViewById(R.id.next);
+        mobile = (EditText) findViewById(R.id.mobile);
 
         Google = (TextView) findViewById(R.id.googleplus);
 
@@ -45,11 +57,40 @@ public class RegisterScreen extends AppCompatActivity {
                 fblogin();
             }
         });
+
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                Toast.makeText(getApplicationContext(),ccp.getSelectedCountryCode()+ "Updated " + ccp.getSelectedCountryName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mobilenumber = String.valueOf(mobile.getText());
+                PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+                try {
+                    Phonenumber.PhoneNumber swissNumberProto = phoneUtil.parse(mobilenumber, ccp.getSelectedCountryNameCode());
+                    boolean isValid = phoneUtil.isValidNumber(swissNumberProto); // returns true
+                    if(isValid)
+                        Log.e("Phone number", swissNumberProto+"");
+                    else
+                        Log.e("Invalid ", "Invalid");
+                } catch (NumberParseException e) {
+                    System.err.println("NumberParseException was thrown: " + e.toString());
+                }
+            validateFeelds();
+            }
+        });
+    }
+
+    private void validateFeelds() {
+
     }
 
 
-
-/** facebook login **/
+    /** facebook login **/
     private void fblogin() {
         try {
             callbackManager = CallbackManager.Factory.create();
