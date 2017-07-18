8ee43f2d979e5a225088ef718940cc5d4cfb7d89
Aneesh P V <aneesh.pv@titechnologies.in>
2017-07-18 11:06:08 +0530

    fblogin

---
+++ app/src/main/java/buddyapp/com/Settings/Constants.java
@@ -0,0 +1,16 @@
+package buddyapp.com.Settings;
+
+
+
+/**
+ * Created by titech on 18/7/17.
+ */
+
+public class Constants {
+
+    public static String fname="first_name";
+    public static String lname= "last_name";
+    public static String email = "email";
+    public static String gender = "gender";
+
+}
--- app/src/main/java/buddyapp/com/activity/RegisterScreen.java
+++ app/src/main/java/buddyapp/com/activity/RegisterScreen.java
@@ -1,16 +1,104 @@
 package buddyapp.com.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import buddyapp.com.R;
+import buddyapp.com.Settings.Constants;
 
 public class RegisterScreen extends AppCompatActivity {
-TextView Google, facebook;
+    TextView Google, facebook;
+    LoginButton facebook_loginbutton;
+    CallbackManager callbackManager;
+    String email, fname, lname, gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+
+        Google = (TextView) findViewById(R.id.googleplus);
+
+        facebook_loginbutton = (LoginButton) findViewById(R.id.login_button);
+        LoginManager.getInstance().logOut();
+        facebook = (TextView) findViewById(R.id.facebook);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                facebook_loginbutton.performClick();
+                fblogin();
+            }
+        });
+    }
+
+
+
+/** facebook login **/
+    private void fblogin() {
+        try {
+            callbackManager = CallbackManager.Factory.create();
+            facebook_loginbutton.setReadPermissions("email");
+            facebook_loginbutton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+               GraphRequest request = GraphRequest.newMeRequest(
+               loginResult.getAccessToken(),
+               new GraphRequest.GraphJSONObjectCallback() {
+                   @Override
+                   public void onCompleted(JSONObject object, GraphResponse response) {
+                     try {
+                           Constants.fname = object.getString("first_name");
+                           Constants.lname = object.getString("last_name");
+                           if (object.getString("gender") != null && object.getString("gender").toString().length() > 0)
+                               Constants.gender = object.getString("gender");
+                           if ((object.toString().contains("email"))) {
+                           if ((object.getString("email")).trim().length() > 0) {
+                               Constants.email = object.getString("email");
+//                                 new checkuserexists().execute();
+                           }
+                           }
+                     }catch (JSONException e) {
+                       e.printStackTrace();
+                       }
+                     }
+               });
+               Bundle parameters = new Bundle();
+               parameters.putString("fields", "id,name,email,gender,birthday,first_name,last_name");
+               request.setParameters(parameters);
+               request.executeAsync();
+        }
+                @Override
+                public void onCancel() {
+                    Log.e("onCancel","onCancel");
+                }
+                @Override
+                public void onError(FacebookException exception) {
+                exception.printStackTrace();
+                }
+      });
+     }catch (Exception e){
+      e.printStackTrace();
+     }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
