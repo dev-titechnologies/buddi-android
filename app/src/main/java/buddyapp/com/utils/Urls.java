d981542b67c093972ebac3f17262ae96c127bb00
Ajay <ajay.k@titechnologies.org>
2017-07-19 11:02:17 +0530

    urls added

---
+++ app/src/main/java/buddyapp/com/utils/Urls.java
@@ -0,0 +1,41 @@
+package buddyapp.com.utils;
+
+/**
+ * Created by root on 19/7/17.
+ */
+
+public class Urls {
+
+    public static String BASEURL= "http://192.168.1.20:9002";
+
+    public static String LOGIN= "/login/login";
+
+    public static String REGISTER= "";
+
+
+    public static String SENDOTP= "";
+
+
+    public static String VERIFYOTP= "";
+
+    public static String getLoginURL(){
+
+        return BASEURL+LOGIN;
+    }
+
+    public static String getRegisterURL(){
+
+        return BASEURL+REGISTER;
+    }
+
+    public static String getSendOTPURL(){
+
+        return BASEURL+SENDOTP;
+    }
+
+    public static String getVerifyOTPURL(){
+
+        return BASEURL+VERIFYOTP;
+    }
+
+}
