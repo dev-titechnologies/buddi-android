7a444b1e97f4e1e742a2f4b6f6fe385958049911
Aneesh P V <aneesh.pv@titechnologies.in>
2017-09-27 18:22:15 +0530

    chnages

--- app/src/main/java/buddyapp/com/activity/Fragment/Help.java
+++ app/src/main/java/buddyapp/com/activity/Fragment/Help.java
@@ -1,11 +1,13 @@
 package buddyapp.com.activity.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import buddyapp.com.R;
 
@@ -13,7 +15,7 @@ import buddyapp.com.R;
  * A simple {@link Fragment} subclass.
  */
 public class Help extends Fragment {
-
+TextView sendMail;
 
     public Help() {
         // Required empty public constructor
@@ -24,7 +26,22 @@ public class Help extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        sendMail = (TextView) view.findViewById(R.id.send_mail);
+
+        sendMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "h@buddiuniverse.com" });
+                startActivity(Intent.createChooser(intent, ""));
+            }
+        });
+
+
+        return view;
     }
 
 }
